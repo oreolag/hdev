948e1a0a34a0adeda66130374f5485613f879472
Javier Moya <jmoya@hey.com>
2025-02-06 10:53:26 +0100

    Update

---
+++ cli/common/is_hyperion_developer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#ToDo: checks if a github user belongs to a TEAM on oreol
+
+
+#inputs
+CLI_PATH=$1
+hostname=$2
+
+#constants
+ACAP_SERVERS_LIST="$CLI_PATH/constants/ACAP_SERVERS_LIST"
+
+#check for acap
+acap="0"
+if (grep -q "^$hostname$" $ACAP_SERVERS_LIST); then
+    acap="1"
+fi
+
+#output
+echo $acap
