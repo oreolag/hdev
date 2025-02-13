a51cd43e58a643f804f0c680818c448145ec1814
Javier Moya <jmoya@hey.com>
2025-02-13 07:58:13 +0100

    Cleanup

--- cli/common/is_composer_developer.sh
+++ cli/common/is_composer_developer.sh
@@ -6,12 +6,6 @@
 #constants
 organization_name="oreolag"
 
-#if [ "$#" -ne 2 ] ; then
-#    echo ""
-#    echo "$0: exactly 2 arguments expected. Example: ./is_composer_developer jmoya82 ETHZ"
-#    exit
-#fi
-
 #gh_check
 logged_in=$($CLI_PATH/common/gh_auth_status)
 if [ "$logged_in" = "0" ]; then 
@@ -21,15 +15,7 @@ fi
 #get gh user
 user_name=$(gh api user --jq '.login')
 
-#check on team first
-#team_exists=$(gh api "/orgs/$organization_name/teams" --paginate | jq -r '.[].slug' | grep -i -w "$team_name")
-#if [ -z "$team_exists" ]; then
-#    echo "0"
-#    exit 1
-#fi
-
 #check on user
-#is_member=$(gh api "/orgs/$organization_name/teams/$team_name/members" --paginate | jq -r '.[].login' | grep -w "$user_name")
 state=$(gh api "/orgs/$organization_name/memberships/$user_name" 2>/dev/null | jq -r '.state' | grep -w "active")
 if [ "$state" = "active" ]; then
     echo "1"
