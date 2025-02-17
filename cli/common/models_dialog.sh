835bffb055d55ccb0f273cd30b1b81fee043317d
Javier Moya <jmoya@hey.com>
2025-02-17 13:12:02 +0100

    Update

--- cli/common/models_dialog.sh
+++ cli/common/models_dialog.sh
@@ -5,6 +5,7 @@ MODELS_PATH=$1
 # Declare global variables
 declare -g model_found="0"
 declare -g model_name=""
+declare -g multiple_models="0"
 
 #get models
 cd "$MODELS_PATH"
@@ -15,6 +16,7 @@ if [ ${#models[@]} -eq 1 ]; then
     model_found="1"
     model_name=${models[0]}
 else
+    multiple_models="1"
     PS3=""
     select model_name in "${models[@]}"; do 
         if [[ -z $model_name ]]; then
@@ -29,3 +31,4 @@ fi
 # Return values
 echo "$platform_found"
 echo "$platform_name"
+echo "$multiple_models"
