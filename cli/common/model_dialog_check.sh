3aedd6986fd0bd2257bf949842799c3aed04ff7b
Javier Moya <jmoya@hey.com>
2025-02-17 12:55:37 +0100

    First commit

---
+++ cli/common/model_dialog_check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+flags=("$@")  # Assign command-line arguments to the 'flags' array
+
+# Declare global variables
+declare -g model_found="0"
+declare -g model_name=""
+
+#read flags
+for (( i=0; i<${#flags[@]}; i++ ))
+do
+    if [[ " ${flags[$i]} " =~ " --model " ]] || if [[ " ${flags[$i]} " =~ " -m " ]]; then
+        model_found="1"
+        model_idx=$(($i+1))
+        model_name=${flags[$model_idx]}
+    fi
+done
+
+#return the values
+echo "$model_found"
+echo "$model_name"
